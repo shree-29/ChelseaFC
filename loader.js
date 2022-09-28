47574229446cb3f1198cddae3ae4bb983ecfbff3
shree-29 <kkshree2910@gmail.com>
2022-09-28 18:32:54 +0530

    json test

--- loader.js
+++ loader.js
@@ -1,26 +1,3 @@
-alert("akd");
-fetch("/news.json")
-.then(function (response) {
-    return response.json();
-})
-.then(function (data) {
-    alert("hi");
-    appendData(data);
-})
-.catch(function (err) {
-    alert("err");
-    console.log('error: ' + err);
-});
-function appendData(data) {
-
-for (var i = 0; i < data.length; i++) {
-    var title = document.getElementById("title-".concat((i+1).toString()));
-    var desc = document.getElementById("desc-".concat((i+1).toString()));
-
-    title.innerHTML=data[i].title;
-    desc.innerHTML=data[i].desc;
-}
-}
 /*
 fetch("/json/trending-topic-btn-text.json")
 .then(function (response) {
